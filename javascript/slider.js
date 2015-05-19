7fddf91e19de821980bd6dbf7f4013f127d6ac04
Sheyna <sheyna@users.noreply.github.com>
2015-05-18 20:42:39 -0700

    removed sample png mockups. Added credit to slider.js

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,3 +1,6 @@
+// From the book "JavaScript & Jquery Interactive
+// Front-end Web Development" by Jon Duckett
+
 $('.slider').each(function() {              // For every slider
   var $this   = $(this);                    // Current slider
   var $group  = $this.find('.slide-group'); // Get the slide-group (container)
